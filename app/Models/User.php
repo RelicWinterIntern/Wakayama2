40ca7e33377517e3187a795a2913b24cb818129f
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-02-22 11:25:23 +0900

    あるあるボタンの追加とデザイン表示の変更をした

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class, 'user_id');
     }
+    public function commonthings()
+    {
+        return $this->hasMany(CommonThing::class, 'user_id');
+    }
 }
